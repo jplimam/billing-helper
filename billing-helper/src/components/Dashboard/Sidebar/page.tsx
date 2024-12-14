68db1bba908799000c4ac8cb5e4c2e5b585885c7
Gabriel Henrique <gabrielhenriquenf1@gmail.com>
2024-12-14 13:15:16 -0300

    fixed homepage sidebar

--- billing-helper/src/components/Dashboard/Sidebar/page.tsx
+++ billing-helper/src/components/Dashboard/Sidebar/page.tsx
@@ -11,32 +11,53 @@ export default function Sidebar() {
     const refHome = useRef<HTMLDivElement>(null)
     const refClients = useRef<HTMLDivElement>(null)
     const refCharges = useRef<HTMLDivElement>(null)
+    const refIndicator = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(refIndicator.current){
+            if(currentSelected == "home" && refHome.current){
+                setIndicatorHeight(Math.floor(refHome.current.getBoundingClientRect().top + (refIndicator.current.clientHeight - refHome.current.clientHeight)/2))
+
+            }
+            if(currentSelected == "clients" && refClients.current){
+                setIndicatorHeight(Math.floor(refClients.current.getBoundingClientRect().top + (refIndicator.current.clientHeight - refClients.current.clientHeight)/2))
+
+            }
+            if(currentSelected == "charges" && refCharges.current){
+                setIndicatorHeight(Math.floor(refCharges.current.getBoundingClientRect().top + (refIndicator.current.clientHeight - refCharges.current.clientHeight)/2))
+
+            }
+            else {
+                //how tf do you get here?
+            }
+        }
+    }, [currentSelected])
     //FIXME: all refs perma returning 128 height, fix that shi
 
     return(
         <div className="bg-gray-900 w-28 h-screen flex flex-row">
             <div className="h-1/2 flex flex-grow flex-col items-center justify-evenly py-6">
-                <div ref={refHome} onClick={() => {setSelected("home"); if(refHome.current){setIndicatorHeight(refHome.current.clientHeight); console.log(refHome.current.clientHeight)}}} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-105 cursor-pointer ${(currentSelected == "home") ? "opacity-100 scale-105" : ""}`}>
-                    <img src="/home-svgrepo-com.svg" alt="home" className="w-auto max-h-[40%]"/>
+                <div ref={refHome} onClick={() => setSelected("home")} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-110 cursor-pointer ${(currentSelected == "home") ? "opacity-95 scale-110" : ""}`}>
+                    <img src="/home-svgrepo-com.svg" alt="home" className="w-auto max-h-[35%]"/>
                     <p className="text-sm text-amber-100">Home</p>
 
                 </div>
 
-                <div ref={refClients} onClick={() => {setSelected("clients"); if(refClients.current){setIndicatorHeight(refClients.current.clientHeight); console.log(refClients.current.clientHeight)}}} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-105 cursor-pointer ${(currentSelected == "clients") ? "opacity-100 scale-105" : ""}`}>
-                    <img src="/users-group-rounded-svgrepo-com.svg" alt="home" className="w-auto max-h-[40%]"/>
+                <div ref={refClients} onClick={() => setSelected("clients")} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-110 cursor-pointer ${(currentSelected == "clients") ? "opacity-95 scale-110" : ""}`}>
+                    <img src="/users-group-rounded-svgrepo-com.svg" alt="home" className="w-auto max-h-[35%]"/>
                     <p className="text-sm text-amber-100">Clientes</p>
 
                 </div>
 
-                <div ref={refCharges} onClick={() => {setSelected("charges"); console.log(refCharges.current?.clientHeight)}} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-105 cursor-pointer ${(currentSelected == "charges") ? "opacity-100 scale-105" : ""}`}>
-                    <img src="/copy-svgrepo-com.svg" alt="home" className="w-auto max-h-[40%]"/>
+                <div ref={refCharges} onClick={() => setSelected("charges")} className={`flex flex-col items-center opacity-50 transition-opacity hover:opacity-100 duration-300 transition-transform hover:scale-110 cursor-pointer ${(currentSelected == "charges") ? "opacity-95 scale-110" : ""}`}>
+                    <img src="/copy-svgrepo-com.svg" alt="home" className="w-auto max-h-[35%]"/>
                     <p className="text-sm text-amber-100">Cobranças</p>
 
 
                 </div>
 
             </div>
-            <div className={`relative w-8 max-h-[10%] bg-amber-100 transition-all duration-300 ease-in-out top-[${indicatorHeight}px]`}>
+            <div ref={refIndicator} className="relative w-8 max-h-[10%] bg-amber-100 transition-all duration-300 ease-in-out" style={{top: `${indicatorHeight}px`}}>
 
             </div>
 
