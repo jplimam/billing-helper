087416639377afffd0048caeb4ee6ed05b97deee
Gabriel Henrique <gabrielhenriquenf1@gmail.com>
2024-12-15 20:46:00 -0300

    Navbar layout finished

--- billing-helper/src/components/Dashboard/Navbar/page.tsx
+++ billing-helper/src/components/Dashboard/Navbar/page.tsx
@@ -1,19 +1,28 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import ProfileOptions from "./profileOptions"
 
 export default function Navbar(){
     const [profileOptions, isProfileOptionsOpen] = useState<boolean>(false)
+    const ref = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(ref.current){
+            document.hasFocus()
+        }
+    }, [])
+
     return(
         <div className="flex h-28 items-center shadow-2xl">
             <h1 className="text-4xl font-semibold ml-10">Home</h1>
 
-            <div className="flex flex-col h-full ml-auto px-10 items-center">
+            <div className="flex flex-col h-full ml-auto px-20 items-center select-none">
 
-                <div className="flex flex-row h-full w-[fit-content] items-center gap-x-5 cursor-pointer" onClick={() => isProfileOptionsOpen(!profileOptions)}>
-                    <img src="./user-icon-placeholder.png" alt="User Icon" className="w-auto h-1/3 rounded-full"/>
-                    <p className="text-sm font-thin">Username</p>
+                <div ref={ref} className="flex flex-row h-full m-auto items-center gap-x-3 cursor-pointer" onFocusCapture={() => isProfileOptionsOpen(!profileOptions)} tabIndex={0} onBlurCapture={() => isProfileOptionsOpen(false)}>
+                    <img src="./user-icon-placeholder.png" alt="User Icon" className="w-auto h-1/2 rounded-full"/>
+                    <p className="text-sm font-thin">username</p>
+                    
                 </div>
 
                 {profileOptions && <ProfileOptions/>}
--- billing-helper/src/components/Dashboard/Navbar/profileOptions.tsx
+++ billing-helper/src/components/Dashboard/Navbar/profileOptions.tsx
@@ -3,25 +3,28 @@ import { motion } from "framer-motion"
 export default function ProfileOptions(){
     return(
         
-        <motion.div className="bg-[#F8FAFC] text-slate-800 my-2 rounded-xl"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1}}>
+        <motion.div className="bg-[#F8FAFC] text-slate-800 my-2 rounded-xl h-[900px]"
+        initial={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ duration: 0.3}}>
             <ul className="">
                 <li className="my-2 mx-4">
-                    <button>
+                    <button className="flex flex-row gap-x-2 items-center">
+                        <img src="/user-circle-svgrepo-com.svg" alt="profile icon" className="w-auto h-5"/>
                         Profile
                     </button>
                 </li>
                 <div className="bg-gray-500 w-4/5 h-[1px] opacity-50 m-auto"/>
                 <li className="my-2 mx-4">
-                    <button>
+                    <button className="flex flex-row gap-x-2 items-center">
+                        <img src="/settings-svgrepo-com.svg" alt="settings icon" className="w-auto h-5"/>
                         Settings
                     </button>
                 </li>
                 <div className="bg-gray-500 w-4/5 h-[1px] opacity-50 m-auto"/>
                 <li className="my-2 mx-4">
-                    <button>
+                    <button className="flex flex-row gap-x-2 items-center text-red-800">
+                        <img src="/user-cross-svgrepo-com.svg" alt="log out icon" className="w-auto h-5"/>
                         Log out
                     </button>
                 </li>
